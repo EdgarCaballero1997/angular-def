f3ce8464433bec5bcd9320045ee29c2ff223ca88
Edgar Caballero <ecaballeromora@outlook.com>
2023-11-30 15:44:17 +0100

    terminado retos dia1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { FormRegisterComponent } from './component/form-register/form-register.component';
+import { RegisterComponent } from './page/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
