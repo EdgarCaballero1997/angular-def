0974cf57e70736daec36743bd5518aa6cc01167d
Edgar Caballero <ecaballeromora@outlook.com>
2023-12-02 02:16:17 +0100

    retos dia2 terminados

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,13 +1,15 @@
 export class User {
 
-    id_user: number;
-    name: string;
-    last_name: string;
-    email: string;
-    photo: string;
-    password: string;
+    public id_user: number;
+    public name: string;
+    public last_name: string;
+    public email: string;
+    public photo: string;
+    public password: string;
 
-    constructor(){
-        
+    constructor(name: string, last_name: string, email: string){
+        this.name = name;
+        this.last_name = last_name;
+        this.email = email;
     }
 }
--- src/app/page/profile/profile.component.ts
+++ src/app/page/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  
+  public user: User;
+  
+  public name: string;
+  public last_name: string;
+  public email: string;
+  public url: string;
 
+  constructor(){
+    this.user = new User(this.name, this.last_name, this.email);
+  }
+
+  infoInputs(name: string, last_name: string, email: string, url: string){
+    this.user.name = name;
+    console.log(this.user.name);
+    this.user.last_name = last_name;
+    this.user.email = email;
+    this.url = url;         // No hay un atributo "URL" en la clase User, no sé qué es exactamente lo que debo hacer, pero hasta que 
+                            // pida ayuda a algún profesor voy a almacenar la URL en un atributo de la clase "ProfileComponent".
+  }
 }
